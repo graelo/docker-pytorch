56811e036a315295801fe9ad30473c4be657a3b8
u0xy <u0xy@u0xy.cc>
2019-12-29 12:45:54 +0100

    add collapsible headings

--- resources/install-python-packages.sh
+++ resources/install-python-packages.sh
@@ -39,7 +39,8 @@ jupyter serverextension enable --py jupyterlab --sys-prefix \
   && jupyter contrib nbextension install --sys-prefix \
   && jupyter labextension install @jupyter-widgets/jupyterlab-manager \
   && jupyter labextension install @oriolmirosa/jupyterlab_materialdarker \
-  && jupyter labextension install @ryantam626/jupyterlab_code_formatter
+  && jupyter labextension install @ryantam626/jupyterlab_code_formatter \
+  && jupyter labextension install @aquirdturtle/collapsible_headings
 
 # conda install -y -c conda-forge jupyterlab_code_formatter
 pip install jupyterlab_code_formatter
